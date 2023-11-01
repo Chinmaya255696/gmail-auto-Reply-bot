6d9db63c2bcd7e4dcfb93be6ab54fec9b9ea4a73
Chinmaya Sahoo <106534765+Chinmaya255696@users.noreply.github.com>
2023-11-02 03:54:37 +0530

    1st commit

---
+++ src/credentials.js
@@ -0,0 +1,6 @@
+
+const CLIENT_ID = "539640497595-nslrl2i58uf9l4fjm3e8bq8db66f49us.apps.googleusercontent.com";
+const CLEINT_SECRET = "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";
+const REDIRECT_URI = "https://developers.google.com/oauthplayground";
+const REFRESH_TOKEN ="1//04rRXptTaW04vCgYIARAAGAQSNwF-L9IrWHEi2dAcKaVR99XsoZ98sAhxk36tIHAFTw4xEBL0ASlijdcik9m0nnmNzlrZRW5Ctik";
+module.exports = { CLIENT_ID, CLEINT_SECRET, REDIRECT_URI, REFRESH_TOKEN };
